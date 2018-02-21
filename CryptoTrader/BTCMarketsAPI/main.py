b08106e2dd9dc5ac7deb4057065a9c5b477e4bd7
Gabriel Varveri <gvarveri@gmail.com>
2018-02-21 15:31:29 +1100

    test

---
+++ CryptoTrader/BTCMarketsAPI/main.py
@@ -0,0 +1,17 @@
+from btcmarkets import BTCMarkets 
+
+import config
+
+api_key = config.api_key
+private_key = config.private_key
+
+client = BTCMarkets (api_key, private_key) 
+
+#print client.trade_history('AUD', 'BTC', 10, 1)
+
+#print client.order_detail([1234, 213456])
+ 
+#print client.order_create('AUD', 'LTC', 100000000, 100000000, 'Bid', 'Limit', '1')
+
+print client.get_market_tick('ETH','AUD')
+
